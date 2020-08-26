cc561bc8707106a292c054ff10b2fd658d43ce4b
Mohammad Dicky Andika Putra <mohammaddicky.andika@gmail.com>
2020-08-26 13:08:17 +0700

    add assignment

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -116,7 +116,7 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       var homeHtmlToInsertIntoMainPage = insertProperty(
-        homeHtmlUrl,
+        homeHtml,
         "randomCategoryShortName",
         chosenCategoryShortName);
 
